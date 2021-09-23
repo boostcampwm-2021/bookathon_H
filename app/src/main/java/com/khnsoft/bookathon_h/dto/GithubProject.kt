bcc744c84876ad3104c66704528a3659e8750979
K.H.Park <svclaw2000@gmail.com>
2021-09-24 03:17:45 +0900

    feat: Get project list from github api

---
+++ app/src/main/java/com/khnsoft/bookathon_h/dto/GithubProject.kt
@@ -0,0 +1,6 @@
+package com.khnsoft.bookathon_h.dto
+
+import kotlinx.serialization.Serializable
+
+@Serializable
+data class GithubProject(val name: String)
--- app/src/main/java/com/khnsoft/bookathon_h/settings/SettingsFragment.kt
+++ app/src/main/java/com/khnsoft/bookathon_h/settings/SettingsFragment.kt
@@ -1,21 +1,32 @@
 package com.khnsoft.bookathon_h.settings
 
+import android.app.AlertDialog
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.EditText
+import androidx.core.view.marginStart
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.activityViewModels
+import com.khnsoft.bookathon_h.R
 import com.khnsoft.bookathon_h.databinding.FragmentSettingsBinding
+import com.khnsoft.bookathon_h.repository.GithubRepository
+import com.khnsoft.bookathon_h.repository.GithubRepositoryImpl
 import com.khnsoft.bookathon_h.viewmodel.ProjectViewModel
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
 
-class SettingsFragment: Fragment() {
+class SettingsFragment : Fragment() {
     private val viewModel: ProjectViewModel by activityViewModels()
     private var _binding: FragmentSettingsBinding? = null
     private val binding get() = _binding!!
 
     private val _temporaryHour = 0
     private val _temporaryMinute = 30
+
+    private val githubRepository: GithubRepository = GithubRepositoryImpl
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         _binding = FragmentSettingsBinding.inflate(inflater, container, false)
@@ -31,6 +42,34 @@ class SettingsFragment: Fragment() {
             maxValue = 59
             value = _temporaryMinute
             wrapSelectorWheel = false
+        }
+
+        binding.githubBtnTextView.setOnClickListener {
+            AlertDialog.Builder(activity).apply {
+                setTitle(R.string.github_username)
+                val view = LayoutInflater.from(activity)
+                    .inflate(R.layout.dialog_github_username, view as ViewGroup, false)
+                setView(view)
+                setNegativeButton(R.string.cancel) { _, _ -> }
+                setPositiveButton(R.string.confirm) { _, _ ->
+                    CoroutineScope(Dispatchers.IO).launch {
+                        try {
+                            val projectNames = githubRepository.getRepositoryNamesOf(
+                                view.findViewById<EditText>(R.id.usernameEditText).text.toString()
+                            )
+                            println(projectNames)
+                        } catch (e: Exception) {
+                            e.printStackTrace()
+                            AlertDialog.Builder(activity).apply {
+                                setTitle(R.string.invalid_username)
+                                setPositiveButton(R.string.confirm) { _, _ -> }
+                                show()
+                            }
+                        }
+                    }
+                }
+                show()
+            }
         }
 
         return binding.root
