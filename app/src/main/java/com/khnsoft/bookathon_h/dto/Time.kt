e05d7874b263b04b2bc4fee9ae5cf006661a3cdf
K.H.Park <svclaw2000@gmail.com>
2021-09-23 16:55:14 +0900

    feat: Add dto for project, task, time

---
+++ app/src/main/java/com/khnsoft/bookathon_h/dto/Time.kt
@@ -0,0 +1,16 @@
+package com.khnsoft.bookathon_h.dto
+
+import kotlinx.serialization.Serializable
+
+@Serializable
+data class Time(val hours: Int = 0, val minutes: Int = 0, val seconds: Int = 0) {
+    companion object {
+        fun fromSeconds(seconds: Int) =
+            Time(seconds / 3600, (seconds % 3600) / 60, seconds % 60)
+    }
+
+    operator fun plus(other: Time) =
+        fromSeconds((hours + other.hours) * 3600 + (minutes + other.minutes) * 60 + (seconds + other.seconds))
+
+    operator fun div(n: Int) = fromSeconds(((hours * 3600 + minutes * 60 + seconds) / n))
+}
