276793af72751740e7588fd0d30b4e410107ebe6
K.H.Park <svclaw2000@gmail.com>
2021-09-24 11:38:17 +0900

    feat: Add project recycler view in Settings

---
+++ app/src/main/java/com/khnsoft/bookathon_h/adapter/SettingsItemDiffCallback.kt
@@ -0,0 +1,17 @@
+package com.khnsoft.bookathon_h.adapter
+
+import androidx.recyclerview.widget.DiffUtil
+
+class SettingsItemDiffCallback: DiffUtil.ItemCallback<SettingsItem>() {
+    override fun areItemsTheSame(oldItem: SettingsItem, newItem: SettingsItem): Boolean {
+        return oldItem == newItem
+    }
+
+    override fun areContentsTheSame(oldItem: SettingsItem, newItem: SettingsItem): Boolean {
+        return when(oldItem) {
+            is SettingsItem.ProjectItem -> oldItem.project.name == (newItem as? SettingsItem.ProjectItem)?.project?.name
+            is SettingsItem.TaskItem -> oldItem.task.name == (newItem as? SettingsItem.TaskItem)?.task?.name
+            is SettingsItem.AddItem -> oldItem == newItem
+        }
+    }
+}
