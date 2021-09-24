27b04bcdeb79ec1bb6a851960c6fb52e64278a27
kldaji <kldaji@naver.com>
2021-09-24 13:20:36 +0900

    [UPDATE] Project and Task to GithubRepo

--- app/src/main/java/com/khnsoft/bookathon_h/dto/Task.kt
+++ app/src/main/java/com/khnsoft/bookathon_h/dto/Task.kt
@@ -1,6 +1,6 @@
-package com.khnsoft.bookathon_h.dto
-
-import kotlinx.serialization.Serializable
-
-@Serializable
-data class Task(val name: String, val time: Time = Time())
+//package com.khnsoft.bookathon_h.dto
+//
+//import kotlinx.serialization.Serializable
+//
+//@Serializable
+//data class Task(val name: String, val time: Time = Time())
--- app/src/main/java/com/khnsoft/bookathon_h/repository/ProjectRepository.kt
+++ app/src/main/java/com/khnsoft/bookathon_h/repository/ProjectRepository.kt
@@ -1,9 +1,9 @@
 package com.khnsoft.bookathon_h.repository
 
 import android.content.Context
-import com.khnsoft.bookathon_h.dto.Project
+import com.khnsoft.bookathon_h.dto.GithubRepo
 
 interface ProjectRepository {
-    fun setProjectList(context: Context, projectList: List<Project>)
-    fun getProjectList(context: Context): List<Project>
+    fun setProjectList(context: Context, projectList: List<GithubRepo.Project>)
+    fun getProjectList(context: Context): List<GithubRepo.Project>
 }
