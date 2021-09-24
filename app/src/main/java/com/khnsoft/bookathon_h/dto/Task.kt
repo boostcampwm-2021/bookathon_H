1fd10644e78b8aee851d1d61972c5e0234b673ac
K.H.Park <svclaw2000@gmail.com>
2021-09-24 12:33:40 +0900

    refactor: Change Task to class

--- app/src/main/java/com/khnsoft/bookathon_h/dto/Task.kt
+++ app/src/main/java/com/khnsoft/bookathon_h/dto/Task.kt
@@ -3,4 +3,15 @@ package com.khnsoft.bookathon_h.dto
 import kotlinx.serialization.Serializable
 
 @Serializable
-data class Task(val name: String, val time: Time = Time())
+class Task(private var _name: String, var _time: Time = Time()) {
+    val name get() = _name
+    val time get() = _time
+
+    fun rename(name: String) {
+        _name = name
+    }
+
+    fun setTime(time: Time) {
+        _time = time
+    }
+}
